44a9a12ddc77a5f22b915d3e8fab61cdf7dfcbdd
suxueshi_lab <1136975295@qq.com>
2020-08-12 14:52:49 +0800

    添加项目文件

---
+++ 6写一个函数求一个字符串的长度.cpp
@@ -0,0 +1,21 @@
+//#include<iostream>
+//#include<string>
+//using namespace std;
+//int length(string s);
+//int main() {
+//	string s;
+//	int len;
+//	cout << "please input string: " << endl;
+//	cin >> s;
+//	len = length(s);
+//	cout << "the length of string is " << len << endl;
+//	cout << "the length of string is " << s.size() << endl;		//.size()就是正常的字符长度	sizeof(s)的话就是所占的字节长度并且包括后面的'\0'
+//	return 0;
+//}
+//int length(string s) {		//不用指针的情况方法  如果使用指针的话要定义字符数组
+//	int n = 0;
+//	while (s[n] != '\0') {
+//		n++;
+//	}
+//	return(n);
+//}
